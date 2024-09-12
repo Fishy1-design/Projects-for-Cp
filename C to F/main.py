d09ac3fcaee7006977e434d7356698cf1bdb646c
Fishy1-design <eva.kibbie@ucas-edu.net>
2024-09-12 10:34:47 -0600

    C to F

--- C to F/main.py
+++ C to F/main.py
@@ -4,3 +4,4 @@
 celsius = int(input("Enter the Temperature in Celsius :\n"))
 fahrenheit = (9/5 * celsius) + 32.
 print("Temperature in Fahrenheit :", fahrenheit)
+print("Temperature in celsius :", celsius)
