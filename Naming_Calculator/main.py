172c755299b58eb47580369609fc101d583bb2b2
Fishy1-design <eva.kibbie@ucas-edu.net>
2024-09-13 10:51:07 -0600

    Calculator

---
+++ Naming_Calculator/main.py
@@ -0,0 +1,7 @@
+num1 = int(input("give me a number birch: "))
+num2 = int(input("give me a second number plz: "))
+
+print(num1,"devided by", num2, "=", num1/num2)
+print(num2, "devided by", num1, "=", num2/num1 )
+print(num1, "multiplied by", num2, "=", num1*num2)
+print(num1, "plus", num2, "=", num1+num2 )
