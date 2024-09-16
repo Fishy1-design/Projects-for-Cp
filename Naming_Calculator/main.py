9ecc47edfffe0f24aaf1cc4ea53cb912d059209f
Fishy1-design <eva.kibbie@ucas-edu.net>
2024-09-16 10:24:52 -0600

    calculator

--- Naming_Calculator/main.py
+++ Naming_Calculator/main.py
@@ -1,6 +1,5 @@
 num1 = int(input("give me a number birch: "))
 num2 = int(input("give me a second number plz: "))
-exponate = num2
 
 print(num1,"devided by", num2, "=", num1/num2)
 print(num2, "devided by", num1, "=", num2/num1 )
@@ -12,3 +11,5 @@ print("exponatal form =", num1**num2)
 print("exponatal form =", num2**num1)
 print(num1, "rounded devision", num2, "=", num1//num2)
 print(num2, "rounded devision", num1, "=", num2//num1)
+print(num1, "modulo", num2, "=", num1%num2)
+print(num2, "modulo", num1, "=", num2%num1)
