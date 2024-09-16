fc938439b50302d8c5b46d857c6f3b745f2f09fd
Fishy1-design <eva.kibbie@ucas-edu.net>
2024-09-16 10:50:30 -0600

    madlib

--- Madlib/main.py
+++ Madlib/main.py
@@ -3,9 +3,10 @@
 w1 = (input("give me a animal: "))
 w2 = (input("give me a verb: "))
 w3 = (input("give me a verb- past tense: "))
-w4 = (input("give me a famous place: "))
-w5 = (input("give me a object: "))
-w6 = (input("give me a job: "))
+w4 = (input("give me another verb- past tense: "))
+w5 = (input("give me a famous place: "))
+w6 = (input("give me a object: "))
+w7 = (input("give me a job: "))
 
-print("today i went to the zoo and saw a ", w1, " it jumed out of its cage and started to", w2, "he", w3, "then he started burning the", w4, "down to the ground with a", w5, "," "the", w6, "statred trying to catch the", w1, "but failed misrably and went home defeated.")
+print("Today I went to the zoo and saw a ", w1, " it jumed out of its cage and started to", w2, "he", w3, "then he started burning the", w5, "down to the ground with a", w6, "," "the", w7, "statred trying to catch the", w1, "but failed misrably and got set on fire with the", w6, "the", w7, "ran around screaming as he", w4, "he then jumped into the", w5, "and put out all the flames. The", w1, "lives to see another day.")
 
