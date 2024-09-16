6b0fa3aa1c3739eda271713ce08c65f492b25f52
Fishy1-design <eva.kibbie@ucas-edu.net>
2024-09-16 10:20:45 -0600

    basic calculator

--- Naming_Calculator/main.py
+++ Naming_Calculator/main.py
@@ -1,7 +1,14 @@
 num1 = int(input("give me a number birch: "))
 num2 = int(input("give me a second number plz: "))
+exponate = num2
 
 print(num1,"devided by", num2, "=", num1/num2)
 print(num2, "devided by", num1, "=", num2/num1 )
 print(num1, "multiplied by", num2, "=", num1*num2)
-print(num1, "plus", num2, "=", num1+num2 )
+print(num1, "plus", num2, "=", num1+num2)
+print(num1, "minus", num2, "=", num1-num2)
+print(num2, "minus", num2, "=", num2-num1)
+print("exponatal form =", num1**num2)
+print("exponatal form =", num2**num1)
+print(num1, "rounded devision", num2, "=", num1//num2)
+print(num2, "rounded devision", num1, "=", num2//num1)
