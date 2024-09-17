26b46e047c64b7911bf7f34eaecc78d116bd9e93
Fishy1-design <eva.kibbie@ucas-edu.net>
2024-09-17 10:50:19 -0600

    compleated quiz

--- graded_quiz/main.py
+++ graded_quiz/main.py
@@ -1,2 +1,45 @@
 #Elisheva Kibbie, graded quiz
 
+print("this is a quiz about Eva and how well you know her ;)")
+score= 0 
+q1 = input("what is evas favorite color?:")
+
+if q1 == "purple": 
+    print("nice.")
+    score +=1 
+else:
+    print("wrong butthole!!")    
+
+    q2 = input("what is evas favorite animal?:")
+
+if q2 == "shark": 
+    print("good job.")
+    score +=1 
+else:
+    print("wrong!!")    
+
+    q3 = input("what is evas favorite food?:")
+
+if q3 == "she doesint have one": 
+    print("sigma.")
+    score +=1 
+else:
+    print("wrong :P!!")     
+
+    q4 = input("what is evas favorite weather?:")
+
+if q4 == "rainy": 
+    print("GYATT.")
+    score +=1 
+else:
+    print("HEHE BUTTHOLE!!") 
+
+    q5 = input("what is evas favorite season?:")
+
+if q5 == "fall": 
+    print("YIPPIE!!")
+    score +=1 
+else:
+    print("WRONGGGGG!!")  
+
+print(score)           
