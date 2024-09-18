73d471ce43d2084182f8f095eb774ec66c506a06
Fishy1-design <eva.kibbie@ucas-edu.net>
2024-09-18 10:38:32 -0600

    personal_info

---
+++ personal_information/main.py
@@ -0,0 +1,17 @@
+#Elisheva Kibbie,m Casating assignment
+
+name = input("What is you name?:")
+age = input("how old are you?:")
+fn = input("what is your favorite number?:")
+height = input("what is your height in feet?:")
+
+print ("processing.......")
+
+name2 = print(name)
+age2 = int(age)
+fn2 = int(fn)
+height2 = float(height)
+
+print("your age went from", age, "to", age2)
+print("your fave num went from", fn, "to", fn2)
+print("and your height went from", height, "to", height2)
