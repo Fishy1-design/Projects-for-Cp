a6ee0297c573c0ebbbafdf9d19f798e6b0c9540e
Fishy1-design <eva.kibbie@ucas-edu.net>
2024-09-19 10:17:14 -0600

    who_are_yoou

--- whoAreYou/main.py
+++ whoAreYou/main.py
@@ -1,7 +1,8 @@
-#elishevakibbie
+#elisheva kibbie, who are you
 
-name = input(" My name is eva")
-print("Name: {:.2f}" .format(name))
 
-amount = 34.67
-print("Amount: ${:.2f}" .format(amount))
+w1 = (input("whats your name?: "))
+w2 = int(input("how are you feeling on a scale of 1-10?: "))
+w3 = (input("whats your favorite animal?: "))
+
+print("Hello,", w1, "im glad your feeling", w2, "/10.","thats cool, i also like", w3)
