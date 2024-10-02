3ed23fb214e2b9049795bb4fecdd3732e271ad3e
Fishy1-design <eva.kibbie@ucas-edu.net>
2024-10-02 10:25:42 -0600

    list:P

---
+++ Shopping_list/main.py
@@ -0,0 +1,24 @@
+#proficiencyTest: Spotting list manager, Elisheva Kibbie 
+
+
+while True:
+
+    action = input("""What would you like to do?
+
+                                  Enter 1 to add item
+
+                                  Enter 2 to remove item
+
+                                  Enter 3 to leave the list:\n""")
+    add= +1
+    remove = -1
+    item = 1
+
+    if action =="1":
+
+        add(+1)
+        remove(-1)
+
+    elseif, action =="2": remove(item)
+    
+else: print("Have a nice day!") 
