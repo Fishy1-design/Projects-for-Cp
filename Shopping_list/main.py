ec00a83e36625bd366257356099810b6572f0914
Eva Kibbie <eva.kibbie@ucas-edu.net>
2024-11-12 18:14:51 -0800

    shpooinlist

--- Shopping_list/main.py
+++ Shopping_list/main.py
@@ -1,29 +1,38 @@
 #proficiencyTest: Spotting list manager, Elisheva Kibbie 
 
-shopping_list = ("start,")
+# Create an empty shopping list
+shopping_list = []
 
-while True:
+# Define function to add an item to the shopping list
+def add():
+    item = input("Whadda ya want?(add): ")
+    shopping_list.append(item)
+    print(f"Stuffs added: {item}")
+    print("Get Da stuffs:", shopping_list)
 
-    action = input("""What would you like to do?
-
-                                  Enter 1 to add item
-
-                                  Enter 2 to remove item
-
-                                  Enter 3 to leave the list:\n""")
-    
-    remove = 
-
-    if action =="1":
-
-        add("squash")
-
-    elif action =="2":
-
-        remove("brocilli")
-
+# Define function to remove an item from the shopping list
+def remove():
+    item = input("I dont want it (remove): ")
+    if item in shopping_list:
+        shopping_list.remove(item)
+        print(f"stuffs thrown onto a random shelf: {item}")
     else:
+        print(f"The thing: '{item}' dont got it. Didja speel its rite?")
+    print("Stuffs i still gotta get:", shopping_list)
 
-        print("Have a nice day!")
+# Main loop that keeps the program running until the user decides to leave
+while True:
+    action = input("""polite buttler: What would you like young miss?
+    Enter 1 to add item
+    Enter 2 to remove item
+    Enter 3 to leave the list:\n""")
 
+    if action == "1":
+        add()
+    elif action == "2":
+        remove()
+    elif action == "3":
+        print("dont let the door hit ya where the good lord split ya!")
         break
+    else:
+        print("FOR SHAME🙀, thats WRONG and out of bounds")
