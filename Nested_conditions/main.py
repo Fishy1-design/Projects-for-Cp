6f4d247c17bd504dd9f1627ca1af6a336e240fdd
Fishy1-design <eva.kibbie@ucas-edu.net>
2024-11-13 10:25:43 -0700

    Sharkquiz

--- Nested_conditions/main.py
+++ Nested_conditions/main.py
@@ -2,45 +2,9 @@
 
 print("this is a quiz about  great white sharks and how well you understand them ;)")
 score= 0 
-q1 = input("what is the prefured tempture for a great white shark?:")
+q1 = input("What are great white sharks prefured water tempetures? (Pick one)")
+input("""
+       A. Cold water
+       B. Warm water 
+       C. Lukewarm water""")
 
-if q1 == "Cold": 
-    print("Correct!")
-    score +=1 
-else:
-    print("wrong.")    
-
-q2 = input("what is evas favorite animal?:")
-
-if q2 == "shark": 
-    print("good job.")
-    score +=1 
-else:
-    print("wrong!!")    
-
-q3 = input("what is evas favorite food?:")
-
-if q3 == "she doesint have one": 
-    print("sigma.")
-    score +=1 
-else:
-    print("wrong :P!!")    
-
-q4 = input("what is evas favorite weather?:")    
-
-if q4 == "rainy": 
-    print("GYATT.")
-    score +=1 
-else:
-    print("HEHE BUTTHOLE!!") 
-
-q5 = input("what is evas favorite season?:")  
-
-if q5 == "fall": 
-    print("YIPPIE!!")
-    score +=1 
-else:
-    print("WRONGGGGG!!")  
-
-print("this your your score:")
-print(score)    
