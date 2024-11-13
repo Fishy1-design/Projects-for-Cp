65a6ae6fa2130ba83385b22287426771d28665cd
Eva Kibbie <eva.kibbie@ucas-edu.net>
2024-11-12 18:17:25 -0800

    shoopinlist

--- Shopping_list/main.py
+++ Shopping_list/main.py
@@ -1,16 +1,16 @@
 #proficiencyTest: Spotting list manager, Elisheva Kibbie 
 
-# Create an empty shopping list
+# Make a list
 shopping_list = []
 
-# Define function to add an item to the shopping list
+# This is for adding stuff to the list
 def add():
     item = input("Whadda ya want?(add): ")
     shopping_list.append(item)
     print(f"Stuffs added: {item}")
     print("Get Da stuffs:", shopping_list)
 
-# Define function to remove an item from the shopping list
+# This is for removing stuff from the list
 def remove():
     item = input("I dont want it (remove): ")
     if item in shopping_list:
@@ -20,7 +20,7 @@ def remove():
         print(f"The thing: '{item}' dont got it. Didja speel its rite?")
     print("Stuffs i still gotta get:", shopping_list)
 
-# Main loop that keeps the program running until the user decides to leave
+# Main loop that runs untill exit
 while True:
     action = input("""polite buttler: What would you like young miss?
     Enter 1 to add item
