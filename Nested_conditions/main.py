8ceb9df95fd53980503307ec78917d20dcac6238
Eva Kibbie <eva.kibbie@ucas-edu.net>
2024-11-13 17:54:24 -0800

    Quizgame

--- Nested_conditions/main.py
+++ Nested_conditions/main.py
@@ -1,10 +1,2 @@
 # nested conditions prof test
 
-print("this is a quiz about  great white sharks and how well you understand them ;)")
-score= 0 
-q1 = input("What are great white sharks prefured water tempetures? (Pick one)")
-input("""
-       A. Cold water
-       B. Warm water 
-       C. Lukewarm water""")
-
