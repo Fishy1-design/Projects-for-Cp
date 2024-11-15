bf7a4b2b047920d5caaaf2ac42677bcc00f5c077
Fishy1-design <eva.kibbie@ucas-edu.net>
2024-11-15 10:30:47 -0700

    buggy

--- Debugger/bug.py
+++ Debugger/bug.py
@@ -1,12 +1,7 @@
-#SkillPractice: Find the Bug 6, Elisheva Kibbie
-
-num = input("what is your number?: ")
-def is_between(num):
-
-     if num > 10 and num < 20:
-
-          return True
-
-     return False
 
 
+#SkillPractice: Find the Bug 1, Elisheva Kibbie
+
+
+def calculate_area(length, width):
+    return length * width
